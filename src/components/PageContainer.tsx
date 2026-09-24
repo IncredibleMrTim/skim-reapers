1f41248d35061f5237228011a46cb5f4ac5886b2
Smart, Tim (The Bridge) <tim.smart@timsmarttechnology.co.uk>
2026-09-24 18:42:30 +0100

    removed page container border-l in mobile

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -16,7 +16,7 @@ export const PageContainer = ({
     <div className={`${floatingHero ? "p-10" : ""}`}>
       {floatingHero ? (
         <section className="relative flex flex-1 pt-20 md:pt-20 w-full">
-          <div className="flex relative z-10 px-0 md:px-8 w-full max-w-[1920px] mx-auto md:ml-(--hero-content-width) md:w-[calc(100%-var(--hero-content-width))] border-l border-l-white/10">
+          <div className="flex relative z-10 px-0 md:px-8 w-full max-w-[1920px] mx-auto md:ml-(--hero-content-width) md:w-[calc(100%-var(--hero-content-width))] border-none md:border-l border-l-white/10">
             {children}
           </div>
         </section>
